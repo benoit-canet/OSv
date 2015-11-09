1b9a0fdbb79005685bf9690d21cbbcd4ac918aa4
Benoît Canet <benoit@scylladb.com>
2015-11-09 18:46:41 +0100

    test: Add test for per namespaces environment variables
    This hopefully show that the feature is working.
    
    Signed-off-by: Benoît Canet <benoit@scylladb.com>

--- tests/tst-namespace.cc
+++ tests/tst-namespace.cc
@@ -17,12 +17,42 @@ void run_namespace()
 
 }
 
-int main(int argc, char **argv)
+void with_namespaces()
 {
     std::thread first = std::thread(run_namespace);
     std::this_thread::sleep_for(std::chrono::seconds(1));
     std::thread second = std::thread(run_namespace);
     first.join();
     second.join();
+}
+
+void test_env()
+{
+    std::vector<std::string> args;
+    std::shared_ptr<osv::application> app;
+    int ret;
+
+    args.push_back("tests/payload-env-namespace.so");
+
+    app = osv::run("/tests/payload-env-namespace.so", args, &ret, true);
+    assert(!ret);
+}
+
+
+void env_with_namespaces()
+{
+    std::thread t = std::thread(test_env);
+    t.join();
+
+    char *value = getenv("FOO");
+    assert(!value);
+}
+
+int main(int argc, char **argv)
+{
+    with_namespaces();
+
+    env_with_namespaces();
+
     return 0;
 }
--- tests/tst-without-namespace.cc
+++ tests/tst-without-namespace.cc
@@ -17,12 +17,41 @@ void run(bool must_succeed)
 
 }
 
-int main(int argc, char **argv)
+void without_namespaces()
 {
     std::thread first = std::thread(run, true);
     std::this_thread::sleep_for(std::chrono::seconds(1));
     std::thread second = std::thread(run, false);
     first.join();
     second.join();
+}
+
+void test_env()
+{
+    std::vector<std::string> args;
+    std::shared_ptr<osv::application> app;
+    int ret;
+
+    args.push_back("tests/payload-env-namespace.so");
+
+    app = osv::run("/tests/payload-env-namespace.so", args, &ret);
+    assert(!ret);
+}
+
+
+void env_without_namespaces()
+{
+    std::thread t = std::thread(test_env);
+    t.join();
+
+    char *value = getenv("FOO");
+    assert(std::string(value) == std::string("BAR"));
+}
+int main(int argc, char **argv)
+{
+    without_namespaces();
+
+    env_without_namespaces();
+
     return 0;
 }
