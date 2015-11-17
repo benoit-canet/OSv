6bf5e8b77bece6a0e423f505195f1e4c36a1d038
Benoît Canet <benoit@scylladb.com>
2015-11-17 16:05:11 +0100

    nfs: Add nfs client and corresponding unit tests

---
+++ fs/nfs/nfs.cc
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2015 Scylla, Ltd.
+ *
+ * Based on ramfs code Copyright (c) 2006-2007, Kohsuke Ohtani
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
+#include <osv/debug.hh>
+
+#include <osv/mount.h>
+
+#include "nfs.hh"
+
+mount_context::mount_context(const char *url)
+    : _valid(false)
+    , _errno(0)
+    , _nfs(nullptr, nfs_destroy_context)
+    , _url(nullptr, nfs_destroy_url)
+{
+    /* Create the NFS context */
+    _nfs.reset(nfs_init_context());
+    if (!_nfs) {
+        debug("mount_context(): failed to create NFS context\n");
+        _errno = ENOMEM;
+        return;
+    }
+
+    // parse the url while taking care of freeing it when needed
+    _url.reset(nfs_parse_url_dir(_nfs.get(), url));
+    if (!_url) {
+        debug(std::string("mount_context():g: ") +
+              nfs_get_error(_nfs.get()) + "\n");
+        _errno = EINVAL;
+        return;
+    }
+
+    _errno = -nfs_mount(_nfs.get(), _url->server, _url->path);
+    if (_errno) {
+        return;
+    }
+
+    _valid = true;
+}
+
+struct mount_context *get_mount_context(struct mount *mp)
+{
+    return static_cast<struct mount_context *>(mp->m_root->d_vnode->v_data);
+}
---
+++ tests-nfs/tst-nfs.cc
@@ -0,0 +1,145 @@
+#include <cassert>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/mount.h>
+#include <fcntl.h>
+
+#include <string>
+
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+void assert_mount_error(int ret)
+{
+    assert(ret != EACCES);
+    assert(ret != EBUSY);
+    assert(ret != EFAULT);
+    assert(ret != EINVAL);
+    assert(ret != ELOOP);
+    assert(ret != EMFILE);
+    assert(ret != ENAMETOOLONG);
+    assert(ret != ENODEV);
+    assert(ret != ENOENT);
+    assert(ret != ENOMEM);
+    assert(ret != ENOTBLK);
+    assert(ret != ENOTDIR);
+    assert(ret != ENXIO);
+    assert(ret != EPERM);
+}
+
+static void test_bogus_url_mount(std::string server, std::string share)
+{
+    std::string mount_point("/bogus");
+    mkdir(mount_point.c_str(), 0777);
+    // Did you notice OSv didn't supported gluster ?
+    std::string url("gluster://" + server + share);
+    int ret = mount(url.c_str(), mount_point.c_str(), "nfs", 0, nullptr);
+    assert(ret && errno == EINVAL);
+}
+
+static void test_mount(std::string server, std::string share,
+                       std::string mount_point)
+{
+    mkdir(mount_point.c_str(), 0777);
+    std::string url("nfs://" + server + share);
+    int ret = mount(url.c_str(), mount_point.c_str(), "nfs", 0, nullptr);
+    if (ret) {
+        int my_errno = errno;
+        std::cout << "Error: " << strerror(my_errno) << "(" << my_errno << ")"
+                  << std::endl;
+        assert_mount_error(my_errno);
+    }
+    assert(!ret);
+}
+
+static void test_umount(std::string &mount_point)
+{
+    int ret = umount(mount_point.c_str());
+    assert(!ret);
+}
+
+static void test_mkdir(std::string mount_point, std::string dir)
+{
+    std::string path = mount_point + "/" + dir;
+    std::cout << "mkdir path " << path << std::endl;
+    int ret = mkdir(path.c_str(), 0755);
+    assert(!ret);
+}
+
+static void test_rmdir(std::string mount_point, std::string dir,
+                       int result, int err_no)
+{
+    std::string path = mount_point + "/" + dir;
+    int ret = rmdir(path.c_str());
+    assert(ret == result);
+    if (ret) {
+        assert(err_no = errno);
+    }
+}
+
+static void test_creat_and_close(std::string mount_point, std::string path)
+{
+    std::string full_path = mount_point + "/" + path;
+    int fd = creat(full_path.c_str(), 0500);
+    assert(fd != -1);
+    int ret = close(fd);
+    assert(!ret);
+}
+
+int main(int argc, char **argv)
+{
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("server", po::value<std::string>(), "set server ip")
+        ("share", po::value<std::string>(), "set remote share")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+
+    std::string server;
+    if (vm.count("server")) {
+        server = vm["server"].as<std::string>();
+    } else {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+
+    std::string share;
+    if (vm.count("share")) {
+        share = vm["share"].as<std::string>();
+    } else {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+
+    test_bogus_url_mount(server, share);
+
+    std::string mount_point("/nfs");
+
+    // Testing mount/umount
+    test_mount(server, share, mount_point);
+    test_umount(mount_point);
+
+    // Testing mkdir and rmdir
+    test_mount(server, share, mount_point);
+    // Test to rmdir something not existing
+    test_rmdir(mount_point, "bar", -1, ENOENT);
+    // mkdir followed by rmdir
+    test_mkdir(mount_point, "foo");
+    //test_rmdir(mount_point, "foo", 0, 0);
+
+    // Testing creat and close
+    test_creat_and_close(mount_point, "zorglub");
+
+    test_umount(mount_point);
+    return 0;
+}
